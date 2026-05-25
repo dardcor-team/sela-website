51e20b17c46c9639abcea825f3ab0dd852aa8f2a
Havid <havidrosi05@gmail.com>
2026-05-26 01:03:56 +0700

    feat : update css

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,6 +14,10 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->redirectGuestsTo('/');
         $middleware->trustProxies(at: '*');
+        $middleware->append(\App\Http\Middleware\CheckMaintenance::class);
+        $middleware->alias([
+            'admin.auth' => \App\Http\Middleware\AdminAuth::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(function ($request) {
